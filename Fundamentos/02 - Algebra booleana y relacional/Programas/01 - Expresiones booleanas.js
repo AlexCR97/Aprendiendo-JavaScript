252e3ee37da622c6852dee1e6f20b100faa9424a
Pablo A. Castillo Ramirez <pablo@dessausoftware.com>
2021-10-27 13:22:04 -0500

    Boolean and Relational expresions programs

---
+++ Fundamentos/02 - Algebra booleana y relacional/Programas/01 - Expresiones booleanas.js
@@ -0,0 +1,10 @@
+/**
+ * Escribe en codigo JavaScript las siguientes expresiones booleanas e imprime el resultado de cada una.
+ * 
+ ** 0 AND 0 OR 1
+ ** 1 AND 1 OR 1 AND 0 OR 0 AND 1
+ ** (1 OR 1) AND (0 OR 1) AND (0 OR 0)
+ ** (1 AND 1) OR (0 AND 1) OR (0 AND 0)
+ ** (1 AND 0) OR ((1 0R 1) AND (0 AND 0))
+ ** 1 AND (0 OR (1 AND ((0 OR 1) OR (1 AND 1))))
+ */
---
+++ Fundamentos/02 - Algebra booleana y relacional/Programas/02 - Expresiones relacionales simples.js
@@ -0,0 +1,16 @@
+/**
+ * Escribe en codigo JavaScript las siguientes expresiones relacionales e imprime el resultado de cada una.
+ * 
+ ** 86 < 100
+ ** 99 < 17
+ ** 20 <= 19
+ ** 48 <= 62
+ ** 51 > 26
+ ** 192 > 237
+ ** 73 >= -11
+ ** 29 >= 29
+ ** -123 == 123
+ ** "123" == 123
+ ** 30 === 30
+ ** 31 === "31"
+ */
