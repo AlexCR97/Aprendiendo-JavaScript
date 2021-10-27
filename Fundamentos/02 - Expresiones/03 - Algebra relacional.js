9ec76189342fbc716f29a4c4162f4d79a6c768f1
Pablo A. Castillo Ramirez <pablo@dessausoftware.com>
2021-10-27 12:59:49 -0500

    Relational algebra

---
+++ Fundamentos/02 - Expresiones/03 - Algebra relacional.js
@@ -0,0 +1,33 @@
+/**
+ * Los operadores del algebra relacional son
+ * 
+ ** <   menor que
+ ** <=  menor o igual que
+ ** >   mayor que
+ ** >=  mayor o igual que
+ ** ==  igual debil (Compara el valor sin tomar en cuenta el tipo de dato. Es menos preciso)
+ ** === igual fuerte (Compara el valor y el tipo de dato. Es mas preciso)
+ */
+
+/**
+ * Los operandos del algebra relacional son tipicamente NUMEROS,
+ * pero se puede utilizar cualquier tipo de dato.
+ * 
+ * Por ahora solo utilizaremos numeros, operaciones relaciones con
+ * otros tipos de dato son un tema mas avanzado.
+ * 
+ ** El resultado de una operacion relacional SIEMPRE va a ser un valor BOOLEANO (true/false)
+ */
+
+/**
+ * Ejemplos de operaciones relacionales
+ * 
+ ** 5 < 10 = VERDADERO
+ ** 10 <= 5 = FALSO
+ ** 8 > 4 = VERDADERO
+ ** 10 >= 10 = VERDADERO
+ ** 1 == 1 = VERDADERO
+ ** '1' == 1 = VERDADERO
+ ** 1 === 1 = VERDADERO
+ ** '1' === 1 = FALSO
+ */
