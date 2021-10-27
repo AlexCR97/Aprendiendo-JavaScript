f99bebb7f49bfcf764f8b115fa7266cf880185c0
Pablo A. Castillo Ramirez <pablo@dessausoftware.com>
2021-10-27 12:41:12 -0500

    Updated boolean algebra docs

--- Fundamentos/02 - Expresiones/01 - algebra booleana.js
+++ Fundamentos/02 - Expresiones/01 - algebra booleana.js
@@ -1,23 +1,44 @@
 /**
  * Los operadores del algebra booleana son AND y OR
+ * 
  * AND = y
  * OR = o
  * 
+ * AND = MULTIPLICACION
+ * OR = SUMA
+ */
+
+
+/**
+ * Los operandos del algebra booleana son 0 y 1
+ * 
  * 0 = FALSO
  * 1 = VERDADERO
+ */
+
+
+/** 
+ * Las operaciones de AND son
  * 
  * 0 AND 0 = 0
  * 0 AND 1 = 0
  * 1 AND 1 = 1
  * 1 AND 0 = 0
+ */
+
+
+/** 
+ * Las operaciones de OR son
  * 
  * 0 OR 0 = 0
  * 0 OR 1 = 1
  * 1 OR 1 = 1
  * 1 OR 0 = 1
- * 
- * AND = MULTIPLICACION
- * OR = SUMA
+ */
+
+
+/**
+ * Ejemplo de operacion con algebra booleana
  * 
  * (0 AND 1) OR (0 AND 0) = 0
  * 0 OR 0 = 0
